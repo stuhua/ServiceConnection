b7ac33e9ae9e515e25d8f768297f1ddb7a3ab4f8
stuhua <850360626@qq.com>
2017-03-23 16:17:15 +0800

    add somethings

--- app/src/main/java/com/stuhua/serviceconnection/MainActivity.java
+++ app/src/main/java/com/stuhua/serviceconnection/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
-
+            mDownloadService = null;
         }
     };
 
